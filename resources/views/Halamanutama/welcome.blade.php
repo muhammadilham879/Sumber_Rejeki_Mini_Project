f7693773812cbcb736a4f7eca721a30cebf08fc7
BagusRayhan <110075643+BagusRayhan@users.noreply.github.com>
2023-06-05 11:54:51 +0700

    Template

---
+++ resources/views/Halamanutama/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        .container{
+            margin-left: 50px;
+            font: 25px sans-serif;
+        }
+        .container-fluid{
+            margin-left: 30px;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SukiSolve</title>
+</head>
+<body>
+    <div class="container">
+    <h1>SukiSolve</h1>
+</div>
+<p class="container-fluid mt-2">Ini adalah halaman utama dari website SukiSolve</p>
+</body>
+</html>
