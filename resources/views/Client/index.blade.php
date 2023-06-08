d0fc412bd01bae398d78e2462510775d6f0ffcd5
ilhamvfx <65000447+muhammadilham879@users.noreply.github.com>
2023-06-08 16:32:08 +0700

    fghn

--- resources/views/Client/index.blade.php
+++ resources/views/Client/index.blade.php
@@ -6,7 +6,7 @@ asdfghjkl;kj
 <head>
 @include('Client.Template.head')
 </head>
-
+gcfvbjhvgcf lkjnbhvgc
 <body>
     <div class="container-xxl position-relative bg-white d-flex p-0">
         <!-- Spinner Start -->
